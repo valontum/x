67cdc9f2b7f11c38ca24136440d24bf282a29ee7
Valon Xhafa <valon.xhafa@tum.de>
2018-01-29 17:44:59 +0100

    cleaning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -45,60 +45,12 @@ import { InboxComponent } from './inbox/inbox.component';
 import { MailService } from "./inbox/mail.service";
 import { InboxComposeComponent } from './inbox/inbox-compose/inbox-compose.component';
 
-import { ChatComponent}  from './chat/chat.component';
-import { CalendarComponent}  from './calendar/calendar.component';
-import { EditorComponent}  from './editor/wysiwyg-editor/editor.component';
-import { Ckeditor } from './editor/ckeditor/ckeditor.component';
+
 import { MaterialIconComponent}  from './material-icons/icons.component';
 import {TransactionsComponent} from './transactions/transactions.component';
 import { ChartComponent}  from './chart/ng2-charts/chart.component';
 import { EasyPieChartComponent}  from './chart/easy-pie-chart/easy-pie-chart.component';
 import {PredictingOverduesComponent} from './predictingoverdues/predictingoverdues.component';
-import { CardsComponent }   from './components/cards/cards.component';
-import { ButtonsComponent }   from './components/buttons/buttons.component';
-import { GridListComponent }  from './components/grid-list/gridlist.component';
-import { ListOverviewComponent }  from './components/list/list.component';
-import { MenuOverviewComponent }  from './components/menu/menu.component';
-import { SliderOverviewComponent }  from './components/slider/slider.component';
-import { SnackbarOverviewComponent }  from './components/snackbar/snackbar.component';
-import { TooltipOverviewComponent }  from './components/tooltip/tooltip.component';
-import { DialogOverviewComponent, DemoDialog}  from './components/dialog/dialog.component';
-import { SelectComponent}  from './components/select/select.component';
-import { InputComponent}  from './components/input/input.component';
-import { CheckboxComponent}  from './components/checkbox/checkbox.component';
-import { RadioComponent}  from './components/radio/radio.component';
-import { ToolbarComponent}  from './components/toolbar/toolbar.component';
-import { ProgressComponent}  from './components/progress/progress.component';
-import { TabsComponent}  from './components/tabs/tabs.component';
-import { ColorpickerComponent}  from './components/colorpicker/colorpicker.component';
-import { DatepickerComponent}  from './components/datepicker/datepicker.component';
-
-import { DragulaDemoComponent}  from './drag-and-drop/dragula/dragula.component';
-import { SortableDemoComponent}  from './drag-and-drop/sortablejs/sortable.component';
-
-import { FullscreenTableComponent}  from './tables/table-fullscreen/table-fullscreen.component';
-import { EditingTableComponent}  from './tables/table-editing/table-editing.component';
-import { FilterTableComponent}  from './tables/table-filter/table-filter.component';
-import { PagingTableComponent}  from './tables/table-paging/table-paging.component';
-import { SortingTableComponent}  from './tables/table-sorting/table-sorting.component';
-import { PinningTableComponent}  from './tables/table-pinning/table-pinning.component';
-import { SelectionTableComponent}  from './tables/table-selection/table-selection.component';
-import { ResponsiveTableComponent}  from './tables/table-responsive/table-responsive.component';
-
-import { FormWizardComponent}  from './forms/form-wizard/formwizard.component';
-import { FormValidationComponent}  from './forms/form-validation/formvalidation.component';
-import { FormUploadComponent}  from './forms/form-upload/formupload.component';
-import { FormTreeComponent}  from './forms/form-tree/formtree.component';
-
-import { GoogleMapComponent}  from './maps/google-map/googlemap.component';
-import { LeafletMapComponent}  from './maps/leaflet-map/leafletmap.component';
-
-import { MediaComponent } from './custom-pages/media/media.component';
-import { PricingComponent } from './custom-pages/pricing/pricing.component';
-import { BlankComponent } from './custom-pages/blank/blank.component';
-
-import { UserProfileComponent } from './users/user-profile/user-profile.component';
-import { UserListComponent } from './users/user-list/userlist.component';
 
 import { LoginComponent } from './session/login/login.component';
 import { RegisterComponent } from './session/register/register.component';
@@ -109,7 +61,7 @@ import { LineChartComponent } from './widgets/line-chart/line-chart.component';
 import { PieChartComponent } from './widgets/pie-chart/pie-chart.component';
 import { StackedAreaChartComponent } from './widgets/stacked-area-chart/stacked-area-chart.component';
 
-import { FileManagerComponent } from './file-manager/file-manager.component'
+
 import {BadDebtPredictionComponent} from './bad debt prediction/baddebtprediction.component';
 import {ChurnPredictionComponent} from './churnprediction/churnprediction.component';
 import {NewPredictionComponent} from './newprediction/newprediction.component';
@@ -185,10 +137,7 @@ const sortablejsConfig: SortablejsOptions = {
         DashboardOneComponent,
         InboxComponent,
         InboxComposeComponent,
-        ChatComponent,
-        CalendarComponent,
-        EditorComponent,
-        Ckeditor,
+       
 		PredictingOverduesComponent,
         BadDebtPredictionComponent,
         ChurnPredictionComponent,
@@ -197,46 +146,7 @@ const sortablejsConfig: SortablejsOptions = {
         ChartComponent,
         NewPredictionComponent,
         EasyPieChartComponent,
-		ButtonsComponent, 
-		CardsComponent, 
-		GridListComponent, 
-		ListOverviewComponent, 
-		MenuOverviewComponent, 
-		SliderOverviewComponent, 
-		SnackbarOverviewComponent, 
-		TooltipOverviewComponent, 
-		DialogOverviewComponent,
-		DemoDialog,
-		SelectComponent,
-		InputComponent,
-		CheckboxComponent,
-		RadioComponent,
-		ToolbarComponent,
-		ProgressComponent,
-		TabsComponent,
-        ColorpickerComponent,
-        DatepickerComponent,
-		DragulaDemoComponent,
-		SortableDemoComponent,
-		FullscreenTableComponent,
-        EditingTableComponent,
-        FilterTableComponent,
-        PagingTableComponent,
-        SortingTableComponent,
-        PinningTableComponent,
-        SelectionTableComponent,
-        ResponsiveTableComponent,
-		FormWizardComponent,
-		FormValidationComponent,
-        FormUploadComponent,
-        FormTreeComponent,
-		GoogleMapComponent,
-		LeafletMapComponent,
-        MediaComponent,
-        UserListComponent,
-        PricingComponent,
-		BlankComponent,
-        UserProfileComponent,
+		
         ReaderChurnComponent,
         LoginComponent,
         RegisterComponent,
@@ -245,11 +155,11 @@ const sortablejsConfig: SortablejsOptions = {
         LineChartComponent,
 		PieChartComponent,
 		StackedAreaChartComponent,
-		nvD3,
-		FileManagerComponent 
+		nvD3
+		
 	],
 	entryComponents: [
-		DemoDialog,
+	
 		InboxComposeComponent,
 	],
 	bootstrap: [GeneAppComponent],
